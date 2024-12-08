e9d39501fc75f4067f373f26e16bdfb48f3c76bb
Juan <guzmanjuan3011@gmail.com>
2024-12-08 14:44:17 -0500

    Link añadidos y maqueteo terminado

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,10 @@
 import '../src/app/globals.css'
+import Script from 'next/script'
+
+export const metadata = {
+  title: "Umbral",
+  description: "Un proyecto de Nicolás Márquez",
+};
 
 export default function RootLayout({ children }) {
     return (
--- app/page.jsx
+++ app/page.jsx
@@ -1,41 +1,48 @@
+import Script from 'next/script'
+
 export default function Page() {
     return (
       <body className="bg-light bg-center bg-cover bg-no-repeat flex h-screen justify-center w-full">
-        <div className="box-border flex flex-col justify-center text-center w-3/4">
-          <div className="flex flex-row text-center">
-            <div className="lg:p-8 p-4 w-1/3">
-            <a href="#" className="font-simonetta neonLink">Link</a>
+        <div className="box-border flex flex-col justify-between py-16 text-center w-3/4">
+          <div className="flex flex-row justify-items-center">
+            <div className="w-1/4">
+              <a href="#">Tráiler</a>
             </div>
-            <div className="lg:p-8 p-4 w-1/3">
-            <a href="#" className="font-simonetta neonLink">Link</a>
+            <div className="w-1/4">
+              <a href="#">Podcast</a>
             </div>
-            <div className="lg:p-8 p-4 w-1/3">
-            <a href="#" className="font-simonetta neonLink">Link 3</a>
+            <div className="w-1/4">
+              <a href="#">Cómic</a>
+            </div>
+            <div className="w-1/4">
+              <a href="#">Álbum</a>
             </div>
           </div>
           <div className="flex flex-row text-center">
-            <div className="lg:py-8 py-4 w-1/3">
-              <a href="https://youtu.be/U0i--wFqmCg" className="font-simonetta neonLink">Tráiler</a>
+            <div className="w-1/3">
+              <a href="https://youtu.be/U0i--wFqmCg" className="font-simonetta neonLink"></a>
             </div>
-            <div className="lg:p-8 p-4 w-1/3">
+            <div className="w-1/3">
               <h1 className="font-bankGothic neonText text-center text-lg">UMBRAL</h1>
             </div>
-            <div className="lg:py-8 py-4 w-1/3">
-            <a href="https://youtu.be/U0i--wFqmCg" className="font-simonetta neonLink">Podcast</a>
+            <div className="w-1/3">
+            <a href="https://youtu.be/U0i--wFqmCg" className="font-simonetta neonLink"></a>
             </div>
           </div>
           <div className="flex flex-row text-center">
-            <div className="lg:p-8 p-4 w-1/3">
-            <a href="#" className="font-simonetta neonLink">Link 6</a>
+            <div className="w-1/2">
+              <a className="font-bankGothic neonLink neonTextSocial text-center text-lg">
+                <i className="fa-brands fa-instagram"></i>
+              </a>
             </div>
-            <div className="lg:p-8 p-4 w-1/3">
-            <a href="#" className="font-simonetta neonLink">Link 7</a>
-            </div>
-            <div className="lg:p-8 p-4 w-1/3">
-            <a href="#" className="font-simonetta neonLink">Link 8</a>
+            <div className="w-1/2">
+              <a className="font-bankGothic neonLink neonTextSocial text-center text-lg">
+                <i className="fa-brands fa-tiktok"></i>
+              </a>
             </div>
           </div>
         </div>
+        <Script src="https://kit.fontawesome.com/umbral.js" />
       </body>
     );
   }
